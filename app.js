a683c1a25a22316f2329d3db9a672d89826ee3a1
Robert Chung <robertchung97@gmail.com>
2016-11-13 01:40:39 -0800

    loading page done, facebook api integrated.

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/bower_components'));
+
 
 app.use('/', index);
 app.use('/users', users);

7df6353568c2e8a21b6eee30c9427d001d9fd757
Robert Chung <robertchung97@gmail.com>
2016-11-12 20:00:02 -0800

    initial commit with express

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var app = express();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+module.exports = router;
